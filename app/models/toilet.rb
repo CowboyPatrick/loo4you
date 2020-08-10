35adb06634cb4fd4e70f51e2b363dbf704039620
Patrick Campbell <campbellpatrickdwight@gmail.com>
2020-08-10 18:06:50 +0900

    added join table bookings

--- app/models/toilet.rb
+++ app/models/toilet.rb
@@ -1,7 +1,9 @@
 class Toilet < ApplicationRecord
   belongs_to :user
+  has_many :bookings
   validates :title, presence: true
   validates :description, presence: true
   validates :category, presence: true
   validates :price, presence: true
+  validates :address, presence: true
 end
