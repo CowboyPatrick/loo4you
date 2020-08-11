89004b7b499eb800de4af547e644e9dd9d9bb23b
Patrick Campbell <campbellpatrickdwight@gmail.com>
2020-08-11 14:21:11 +0900

    booking controller index and skelton index view

--- app/controllers/toilets_controller.rb
+++ app/controllers/toilets_controller.rb
@@ -1,8 +1,8 @@
 class ToiletsController < ApplicationController
 
   def index
-   @toilets = Toilet.all
- end
+    @toilets = policy_scope(Toilet)
+  end
 
   def show
     @toilet = Toilet.find(params[:id])
