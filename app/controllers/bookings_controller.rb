526515a3511f89b9320c92c8bc328b0517e337c4
Patrick Campbell <campbellpatrickdwight@gmail.com>
2020-08-11 17:02:23 +0900

    added a flush button to make a booking on toilet show page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,4 +3,25 @@ class BookingsController < ApplicationController
   def index
     @bookings = policy_scope(Booking)
   end
+
+
+  def create
+    @booking = Booking.new
+    @toilet = Toilet.find(params[:toilet_id])
+    @booking.toilet = @toilet
+    @user = current_user
+    @booking.user = @user
+    authorize @booking
+    if @booking.save
+      redirect_to toilet_path(@toilet), notice: "Successful booking"
+    else
+      render 'toilets/show'
+    end
+  end
+
+  # private
+
+  # def booking_params
+  #   params.require(:booking).permit(:user_id, :toilet_id)
+  # end
 end
