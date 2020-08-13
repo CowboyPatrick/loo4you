40d13e757b0aac4b853b0ff4bef69e157db967c8
Jocelyn Yuan <jocelyn19950627@yahoo.com.tw>
2020-08-13 11:33:38 +0900

    image

--- app/controllers/toilets_controller.rb
+++ app/controllers/toilets_controller.rb
@@ -2,17 +2,7 @@ class ToiletsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-    if params[:query].present?
-      sql_query = " \
-        toilets.title @@ :query \
-        OR movies.description @@ :query \
-        OR directors.first_name @@ :query \
-        OR directors.last_name @@ :query \
-      "
-      @movies = Toilet.where(sql_query, query: "%#{params[:query]}%")
-    else
-      @toilets = policy_scope(Toilet)
-    end
+    @toilets = policy_scope(Toilet)
   end
 
   def show
