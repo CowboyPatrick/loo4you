ab9d132be9f30384c7de53dba3bd5934188ce42a
Patrick Campbell <campbellpatrickdwight@gmail.com>
2020-08-14 12:40:53 +0900

    added the beginning of our map markers

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -5,22 +5,24 @@ import mapboxgl from 'mapbox-gl';
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 5000 });
 };
 
 
 
 
 const  addMarkers = (mapElement, map) => {
-const markers = JSON.parse(mapElement.dataset.markers);
+  const markers = JSON.parse(mapElement.dataset.markers);
+
   markers.forEach((marker) => {
+    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
     new mapboxgl.Marker()
       .setLngLat([ marker.lng, marker.lat ])
+      .setPopup(popup)
       .addTo(map);
   });
+  return markers
 }
-
-
 
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
@@ -31,7 +33,8 @@ const initMapbox = () => {
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10'
     });
-    addMarkers(mapElement, map);
+    const markers = addMarkers(mapElement, map);
+    fitMapToMarkers(map, markers );
   }
 };
 
